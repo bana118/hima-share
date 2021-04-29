5658872a36d2e8aa25f9c676efec7c0ee3e24eb6
bana118 <bana.titech@gmail.com>
2021-04-29 04:10:08 +0000

    check git tag on PR

---
+++ scripts/git_tag_checker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Check if the version of package.json is already in the git tag
+VERSION=$(npx -c 'echo "$npm_package_version"')
+COUNT=$(git tag | grep -c ${VERSION})
+
+if [ ${COUNT} -eq 0 ]; then
+    printf '%s\n' "OK v${VERSION} does not exist" >&1
+    exit 0
+else
+    printf '%s\n' "ERROR! v${VERSION} already exists" >&2
+    exit 1
+fi
