db3b8e5bff5f79e9820491d231c0e7f9d5082250
bana118 <bana.titech@gmail.com>
2021-05-01 19:13:51 +0900

    add yup and use yup validation at register form

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,38 +1,85 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Form, Button } from "react-bootstrap";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+type InputsType = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("メールアドレスの形式に誤りがあります")
+    .required("メールアドレスは必須です"),
+  password: yup
+    .string()
+    .min(8, "パスワードは8文字以上に設定してください")
+    .required("パスワードは必須です"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "パスワードが一致しません")
+    .required("パスワードは必須です"),
+});
 
 export const RegisterForm = (): JSX.Element => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const regesterUser = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(email);
-    console.log(password);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputsType>({ resolver: yupResolver(schema) });
+  const registerUser = (data: InputsType) => {
+    console.log(data);
   };
   return (
-    <Form onSubmit={regesterUser}>
-      <Form.Group controlId="formBasicEmail">
+    <Form onSubmit={handleSubmit(registerUser)}>
+      <Form.Group>
         <Form.Label>Email address</Form.Label>
         <Form.Control
           type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          isInvalid={!!errors.email}
+          {...register("email")}
         />
-        <Form.Text className="text-muted">
-          We&#39;ll never share your email with anyone else.
-        </Form.Text>
+        {errors.email && (
+          <Form.Control.Feedback type="invalid">
+            {errors.email.message}
+          </Form.Control.Feedback>
+        )}
       </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
+      <Form.Group>
         <Form.Label>Password</Form.Label>
         <Form.Control
           type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
+          isInvalid={!!errors.password}
+          {...register("password")}
         />
+        <Form.Text className="text-muted">
+          パスワードは8文字以上に設定してください
+        </Form.Text>
+        {errors.password && (
+          <Form.Control.Feedback type="invalid">
+            {errors.password.message}
+          </Form.Control.Feedback>
+        )}
       </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Confirm password</Form.Label>
+        <Form.Control
+          type="password"
+          isInvalid={!!errors.confirmPassword}
+          {...register("confirmPassword")}
+        />
+        {errors.confirmPassword && (
+          <Form.Control.Feedback type="invalid">
+            {errors.confirmPassword.message}
+          </Form.Control.Feedback>
+        )}
+      </Form.Group>
+
       <Button variant="primary" type="submit">
         Submit
       </Button>
