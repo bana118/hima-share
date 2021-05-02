89c04a1a2b9b8e08f17cd3a92e08b9a7ff182165
bana118 <bana.titech@gmail.com>
2021-05-02 08:46:47 +0000

    use roboto font for alphabet

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -27,7 +27,7 @@ const Layout = ({ children, title = "Default Title" }: Props): JSX.Element => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto&display=swap"
           rel="stylesheet"
         />
       </Head>
