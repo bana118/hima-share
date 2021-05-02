258971e93e91382fc4f6285994a18b243093562d
bana118 <bana.titech@gmail.com>
2021-05-02 23:33:57 +0900

    check busy day as B

--- components/StatusCalender.tsx
+++ components/StatusCalender.tsx
@@ -1,26 +1,39 @@
 import moment from "moment";
+import { DayPicker } from "react-dates";
 
 type Status = "free" | "busy" | "undecided";
 
-type dateStatus = {
+export type DateStatus = {
   date: moment.Moment;
   status: Status;
 };
 
 type StatusCalenderProps = {
-  dateStatusList: dateStatus[];
-  setDateStatusList: (list: dateStatus[]) => void;
+  dateStatusList: DateStatus[];
+  setDateStatusList: (list: DateStatus[]) => void;
 };
 
 export const StatusCalender = ({
   dateStatusList,
   setDateStatusList,
-}: StatusCalenderProps): JSX.Element => (
-  <ul>
-    {items.map((item) => (
-      <li key={item.id}>
-        <ListItem data={item} />
-      </li>
-    ))}
-  </ul>
-);
+}: StatusCalenderProps): JSX.Element => {
+  const updateDateStatusList = (day: moment.Moment) => {
+    console.log(dateStatusList);
+    const index = dateStatusList.findIndex((ds) => ds.date === day);
+    if (index == -1) {
+      const newDateStatus: DateStatus = { date: day, status: "busy" };
+      setDateStatusList([...dateStatusList, newDateStatus]);
+    }
+  };
+  return (
+    <DayPicker
+      numberOfMonths={1}
+      onDayClick={updateDateStatusList}
+      renderDayContents={(day) => {
+        const dateStatus = dateStatusList.find((ds) => ds.date === day);
+        const isBusy = dateStatus != null && dateStatus.status == "busy";
+        return isBusy ? "B" : day.format("D");
+      }}
+    />
+  );
+};
--- pages/calender.tsx
+++ pages/calender.tsx
@@ -3,19 +3,19 @@ import { useState } from "react";
 
 import "react-dates/initialize";
 import "react-dates/lib/css/_datepicker.css";
-import moment from "moment";
-import { DayPicker } from "react-dates";
+import { DateStatus } from "../components/StatusCalender";
+import { StatusCalender } from "../components/StatusCalender";
 
 const CalenderPage = (): JSX.Element => {
-  const [date, setDate] = useState<moment.Moment | null>(moment);
-  const [focused, setFocused] = useState<boolean>(false);
+  const [dateStatusList, setDateStatusList] = useState<DateStatus[]>([]);
   return (
     <Layout title="Calender example">
       <h1>Calender</h1>
       <p>Calender example</p>
-      <div>
-        <DayPicker numberOfMonths={1} />
-      </div>
+      <StatusCalender
+        dateStatusList={dateStatusList}
+        setDateStatusList={(list: DateStatus[]) => setDateStatusList(list)}
+      />
     </Layout>
   );
 };
