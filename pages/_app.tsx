86859fa8854f2e9e89fe5ce1bc97838d81e882de
bana118 <bana.titech@gmail.com>
2021-05-03 01:07:16 +0900

    reove moment setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ import firebase from "firebase/app";
 import { auth } from "../utils/firebase";
 import { AuthContext } from "../context/AuthContext";
 import "../styles/sass/custom.scss";
-import moment from "moment";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   const [user, setUser] = useState<firebase.User | null>(null);
@@ -16,9 +15,6 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
       setUser(u);
     });
   }, []);
-
-  // Set global localization for calender
-  moment.locale("ja");
 
   return (
     <AuthContext.Provider value={{ user }}>
