be0d691ee93e80ccf2b589a04c70bf8fde8428bf
bana118 <bana.titech@gmail.com>
2021-05-06 04:20:45 +0900

    redirect login user to index on login and register page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,31 @@
 import Link from "next/link";
 import Router from "next/router";
+import React, { useEffect, useContext } from "react";
 import Layout from "../components/Layout";
 import { LoginForm } from "../components/LoginForm";
+import { AuthContext } from "../context/AuthContext";
 
 const LoginPage = (): JSX.Element => {
+  const { authUser } = useContext(AuthContext);
+  useEffect(() => {
+    if (authUser !== null) {
+      Router.push("/");
+    }
+  }, [authUser]);
   return (
-    <Layout title="Login">
-      <h1>Login</h1>
-      <LoginForm onLogined={() => Router.push("/")} />
-      <p>
-        <Link href="/register">
-          <a>登録</a>
-        </Link>
-      </p>
-    </Layout>
+    <React.Fragment>
+      {authUser === null && (
+        <Layout title="Login">
+          <h1>Login</h1>
+          <LoginForm onLogined={() => Router.push("/")} />
+          <p>
+            <Link href="/register">
+              <a>登録</a>
+            </Link>
+          </p>
+        </Layout>
+      )}
+    </React.Fragment>
   );
 };
 
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,19 +1,31 @@
 import Link from "next/link";
 import Router from "next/router";
+import React, { useEffect, useContext } from "react";
 import Layout from "../components/Layout";
 import { RegisterForm } from "../components/RegisterForm";
+import { AuthContext } from "../context/AuthContext";
 
 const RegisterPage = (): JSX.Element => {
+  const { authUser } = useContext(AuthContext);
+  useEffect(() => {
+    if (authUser !== null) {
+      Router.push("/");
+    }
+  }, [authUser]);
   return (
-    <Layout title="Register">
-      <h1>Register</h1>
-      <RegisterForm onRegistered={() => Router.push("/calendar")} />
-      <p>
-        <Link href="/login">
-          <a>ログイン</a>
-        </Link>
-      </p>
-    </Layout>
+    <React.Fragment>
+      {authUser === null && (
+        <Layout title="Register">
+          <h1>Register</h1>
+          <RegisterForm onRegistered={() => Router.push("/calendar")} />
+          <p>
+            <Link href="/login">
+              <a>ログイン</a>
+            </Link>
+          </p>
+        </Layout>
+      )}
+    </React.Fragment>
   );
 };
 
