c9b7560028599b6a4dfa39f36f5f29c38bb02519
bana118 <bana.titech@gmail.com>
2021-05-07 21:36:03 +0900

    show date string on tooltip

--- components/GroupCalendar.tsx
+++ components/GroupCalendar.tsx
@@ -1,3 +1,5 @@
+import React, { useRef, useState } from "react";
+import { Overlay, Tooltip } from "react-bootstrap";
 import Calendar from "react-calendar";
 import { UserDateStatusList } from "../interfaces/DateStatus";
 
@@ -13,6 +15,9 @@ type DateToNum = {
 export const GroupCalendar = ({
   groupDateStatusList: groupDateStatusList,
 }: GroupCalendarProps): JSX.Element => {
+  const calendarRef = useRef(null);
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [focusedDate, setFocusedDate] = useState<Date | undefined>(undefined);
   // TODO "dd日" ではなく "dd" だけ表示する
   const dateToFreeNumList: DateToNum[] = [];
   for (const userDateStatusList of groupDateStatusList) {
@@ -42,31 +47,56 @@ export const GroupCalendar = ({
     }
   };
   return (
-    <Calendar
-      locale="ja-JP"
-      minDate={new Date()}
-      minDetail="month"
-      maxDetail="month"
-      defaultView="month"
-      showNavigation={true}
-      prev2Label={null}
-      next2Label={null}
-      tileContent={({ date }) => {
-        const freeNum = countFreeNum(date);
-        return <div>{freeNum}</div>;
-      }}
-      tileClassName={({ date }) => {
-        const freeNum = countFreeNum(date);
-        // TODO 暇な人の人数でスタイルを変えるための基準人数を設定可能にする
-        // TODO 色はグラデーションでもいいかもしれない
-        if (freeNum >= 5) {
-          return "calendar-free";
-        } else if (freeNum >= 2) {
-          return "calendar-light-free";
-        } else {
-          return null;
-        }
-      }}
-    />
+    <React.Fragment>
+      <div></div>
+      <Calendar
+        inputRef={calendarRef}
+        onClickDay={(date) => {
+          setFocusedDate(date);
+          setShowTooltip(true);
+        }}
+        locale="ja-JP"
+        minDate={new Date()}
+        minDetail="month"
+        maxDetail="month"
+        defaultView="month"
+        showNavigation={true}
+        prev2Label={null}
+        next2Label={null}
+        tileContent={({ date }) => {
+          const freeNum = countFreeNum(date);
+          return <div>{freeNum}</div>;
+        }}
+        tileClassName={({ date }) => {
+          const freeNum = countFreeNum(date);
+          // TODO 暇な人の人数でスタイルを変えるための基準人数を設定可能にする
+          // TODO 色はグラデーションでもいいかもしれない
+          if (freeNum >= 5) {
+            return "calendar-free";
+          } else if (freeNum >= 2) {
+            return "calendar-light-free";
+          } else {
+            return null;
+          }
+        }}
+      />
+      <Overlay
+        target={calendarRef.current}
+        show={showTooltip}
+        placement="right"
+      >
+        {(props) => (
+          <Tooltip
+            onClick={() => {
+              setShowTooltip(false);
+            }}
+            id="overlay-example"
+            {...props}
+          >
+            {focusedDate?.toString()}
+          </Tooltip>
+        )}
+      </Overlay>
+    </React.Fragment>
   );
 };
