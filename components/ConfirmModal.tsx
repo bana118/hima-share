6695cd21b6d32dd8716ab2c69d4647131d179099
bana118 <bana.titech@gmail.com>
2021-05-10 18:59:54 +0900

    add confirm modal component

---
+++ components/ConfirmModal.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+
+interface ConfirmModalProps {
+  show: boolean;
+  setShow: (show: boolean) => void;
+  headerText: string;
+  bodyText: string;
+  confirmButtonText: string;
+  onClickConfirmButton: () => void;
+}
+
+export const ConfirmModal = ({
+  show,
+  setShow,
+  headerText,
+  bodyText,
+  confirmButtonText,
+  onClickConfirmButton,
+}: ConfirmModalProps): JSX.Element => {
+  return (
+    <Modal show={show} onHide={() => setShow(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>{headerText}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{bodyText}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => setShow(false)}>
+          キャンセル
+        </Button>
+        <Button variant="main" onClick={onClickConfirmButton}>
+          {confirmButtonText}
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
