69947b70d8e0e7dbbc96aeb385b41991051b84de
bana118 <bana.titech@gmail.com>
2021-05-14 23:07:55 +0900

    remove preconnect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,8 +19,9 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
     auth.onAuthStateChanged((u) => {
       setAuthUser(u);
     });
-    console.log(process.env.NODE_ENV);
+
     if (process.env.NODE_ENV === "production") {
+      console.log(process.env.NODE_ENV);
       analytics();
     }
   }, []);
