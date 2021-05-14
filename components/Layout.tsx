3023ae26e87d0adafde21aa5691581de9724dfe0
bana118 <bana.titech@gmail.com>
2021-05-14 20:18:00 +0900

    add canonical and ogp to head

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,6 +18,8 @@ export const Layout = ({
   title = "Default Title",
 }: Props): JSX.Element => {
   const { authUser } = useContext(AuthContext);
+  const url = document.location.href;
+  const twiiterId = process.env.NEXT_PUBLIC_TWITTER_ID;
   const logout = async (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
     event.preventDefault();
     // auth.signOut().then(() => { Router.puth("/") }) とやると他ページの/loginへのリダイレクトが先に働いてしまう
@@ -35,10 +37,22 @@ export const Layout = ({
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="canonical" href={url} />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto&display=swap"
           rel="stylesheet"
         />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content={url} />
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="og:title" content={title} />
+        <meta
+          property="og:description"
+          content="あなたの暇な日をシェアしよう"
+        />
+        <meta property="og:image" content="/about1.png" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content={twiiterId} />
       </Head>
       <header>
         <Navbar variant="dark" className="hima-share-navbar">
