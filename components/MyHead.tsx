e7eb274d993bbd501e9593b1323a0f5b4f44b3b4
bana118 <bana.titech@gmail.com>
2021-05-14 23:23:20 +0900

    add preconnect

--- components/MyHead.tsx
+++ components/MyHead.tsx
@@ -15,6 +15,7 @@ export const MyHead = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="canonical" href={url} />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto&display=swap"
         rel="stylesheet"
