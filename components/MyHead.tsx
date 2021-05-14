5223dfea24c499fd0ec37a619761901e50965c07
bana118 <bana.titech@gmail.com>
2021-05-15 00:01:10 +0900

    fix ogp image url

--- components/MyHead.tsx
+++ components/MyHead.tsx
@@ -8,17 +8,18 @@ export const MyHead = ({
   title = "Default Title",
 }: MyHeadProps): JSX.Element => {
   const url = typeof window !== "undefined" ? document.location.href : "";
+  const origin = typeof window !== "undefined" ? document.location.origin : "";
   const twiiterId = process.env.NEXT_PUBLIC_TWITTER_ID;
   return (
     <Head>
       <title>{title}</title>
       <link rel="canonical" href={url} />
-      <meta property="og:type" content="article" />
+      <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
       <meta property="og:locale" content="ja_JP" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content="あなたの暇な日をシェアしよう" />
-      <meta property="og:image" content="/about1.png" />
+      <meta property="og:image" content={`${origin}/about1.png`} />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content={twiiterId} />
     </Head>
